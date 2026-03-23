81bd4f84751cad5c80d087099fb13f4eff9e3a71
Ezekiel544 <chikezieezekiel3@gmail.com>
2026-03-23 14:54:13 +0100

    new changes added

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -44,7 +44,7 @@ export function Experience() {
           ))}
         </div>
 
-        {/* Experience Description added here*/}
+        {/* Experience Description added here now*/}
         <motion.div
           initial={{ opacity: 0, y: 30 }}
           animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 30 }}
