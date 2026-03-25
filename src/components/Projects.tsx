11e4ee0b154b0f844daa039be3b8f91b0d8cad3a
Ezekiel544 <chikezieezekiel3@gmail.com>
2026-03-25 14:41:57 +0100

    new changes added

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -21,7 +21,7 @@ export function Projects() {
     // {
     //   name: "Ugly Ducks here",
     //   description:
-    //     "Supported their NFT sales campaign, helping them sell out nearly their entire collection. hhhggh",
+    //     "Supported their NFT sales campaign, helping them sell out nearly their entire collection. hhgh",
     //   icon: Image,
     //   status: "Completed",
     // },
