4aa65fb5afe1ad909750bac519112378b641319e
Ezekiel544 <chikezieezekiel3@gmail.com>
2026-03-25 14:32:35 +0100

    new changes added

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -129,7 +129,7 @@ export function Socials() {
             </div>
           </motion.div>
 
-          {/* EVM Address added hh */}
+          {/* EVM Address added h*/}
           <motion.div
             initial={{ opacity: 0, x: 30 }}
             animate={isInView ? { opacity: 1, x: 0 } : { opacity: 0, x: 30 }}
