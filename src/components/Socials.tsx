2aeb52cb95ef60d2af9b7216e0f93436cda4e73c
Ezekiel544 <chikezieezekiel3@gmail.com>
2026-03-25 14:34:27 +0100

    new changes added

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -87,7 +87,7 @@ export function Socials() {
           </div>
         </motion.div>
 
-        {/* On-Chain Identity nnnn*/}
+        {/* On-Chain Identity nn*/}
         <motion.h2
           initial={{ opacity: 0, y: 30 }}
           animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 30 }}
