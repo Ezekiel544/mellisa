36d22d4c17583dac9caa12a49386090048cc1d31
Ezekiel544 <chikezieezekiel3@gmail.com>
2026-03-25 14:18:03 +0100

    new changes added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -116,7 +116,7 @@ export function Hero() {
             ))}
           </motion.div>
 
-          {/* Scroll indicator added*/}
+          {/* Scroll indicator*/}
           <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
